64dc2847657fc925e087c19bb52f0c388a9dae4a
Choquidownn25 <jose_sarria2003@yahoo.com.mx>
2023-12-21 11:22:59 -0500

    Subo para commit consumer del auto commit

--- demo-kafka-autocommint/src/main/java/com/example/demokafkaautocommint/service/KafkaConsumer.java
+++ demo-kafka-autocommint/src/main/java/com/example/demokafkaautocommint/service/KafkaConsumer.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 @Service
@@ -19,15 +20,13 @@ public class KafkaConsumer {
     @Value("${spring.kafka.bootstrap-servers}")
     private String servers; // Value = 'SpringAppCluste
     @KafkaListener(topics = "devs4j-topic", groupId = "group_id", containerFactory = "kafkaListenerContainerFactory")
-    public void consume(ConsumerRecord<String, String> record) {
-        //int numero = record.key().length();
-        log.info("Consumer Menssage : ", record.value());
+    public void consume(ConsumerRecord<Integer, String> record) {
+
         Properties props=new Properties();
-        //props.put("bootstrap.servers", "localhost:9092");
         props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
-        props.setProperty("group.id","devs4j-group"); //Multiples clientes
-       // props.setProperty("enable.auto.commit","true");
-        //props.setProperty("auto.commit.interval.ms","1000");
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG,"devs4j-group"); //Multiples clientes
+        props.setProperty("enable.auto.commit","true");
+        props.setProperty("auto.commit.interval.ms","1000");
         props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
                 "org.apache.kafka.common.serialization.StringDeserializer");
         props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
@@ -35,9 +34,11 @@ public class KafkaConsumer {
         props.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, String.valueOf(false));
         try(org.apache.kafka.clients.consumer.KafkaConsumer<String, String> consumer=new
                 org.apache.kafka.clients.consumer.KafkaConsumer<>(props);) {
-            consumer.subscribe(Arrays.asList("devs4j-topic"));
+            consumer.subscribe(List.of("devs4j-topic"));
+            System.out.println("Cantidad : "+ record.leaderEpoch().get().intValue() ) ;
 
-            while(true) {
+
+            for(int i=0; i<=record.leaderEpoch().get().intValue(); i++) {
                 ConsumerRecords<String,String> records=
                         consumer.poll(Duration.ofMillis(1));
                 for(ConsumerRecord<String, String> recordt
@@ -45,13 +46,10 @@ public class KafkaConsumer {
                     log.info("offset = {}, key = {}, value ={}",
                             record.offset(),record.key(),record.value());
                 consumer.commitSync();
+                log.info("Consumer Menssage : ", record.value());
                 System.out.println("Consumed message or Received Message : " + record.value());
             }
         }
-
-            //System.out.println("Consumed message or Received Message : " + record.value());
-
-
 
     }
 }
